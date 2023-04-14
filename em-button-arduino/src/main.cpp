1202a9c457e59827bb19dc0cf79a40f1ece44128
Joshua Fletcher <jfletcher20@student.foi.hr>
2023-04-14 12:44:30 +0200

    Implemented working bluetooth serial port. To monitor must monitor, must add additional connection separate from BleKeyboard in bluetooth and monitor the new port (for example, COM9).

--- em-button-arduino/src/main.cpp
+++ em-button-arduino/src/main.cpp
@@ -1,18 +1,81 @@
+#include <BluetoothSerial.h>
+#include <BleKeyboard.h>  
 #include <Arduino.h>
+#include <stdio.h>
+#include "String.h"
 
-#define BUTTON_PIN 21
+#define BUTTON_PIN 2
+
+BleKeyboard bleKeyboard;
+BluetoothSerial SerialBT;
 
 void setup() {
+
   pinMode(BUTTON_PIN, INPUT);
   Serial.begin(115200);
+  SerialBT.begin("ESP32 Keyboard (Serial)");
+
+  // TODO: look into following link for possible solutions on device multiconnect:
+  // https://community.appinventor.mit.edu/t/ble-esp32-bluetooth-multiconnect-connect-two-mobiles-at-the-same-time/44357/3
+  bleKeyboard.begin();
+
+  Serial.println("Searching for connections...");
+
 }
 
+void connectionStatus();
+
+bool keylock = 0;
+bool isConnected = 0;
+unsigned long blockTime = 0;
+
 void loop() {
-  int buttonState = digitalRead(BUTTON_PIN);
-  if (buttonState == LOW) {
-    Serial.println("off");
-  } else {
-    Serial.println("on");
+
+  if (SerialBT.available() > 0) {
+    String c = SerialBT.readString();
+    c.trim();
+    Serial.println("Received: " + c);
   }
-  delay(100); // add a small delay to avoid bouncing
+
+
+  connectionStatus();
+
+  if(digitalRead(BUTTON_PIN) == LOW && keylock) {
+    if(blockTime == 0) {
+      blockTime = millis();
+    } else {
+      if(millis() - blockTime >= 100)
+        keylock = blockTime = 0;
+    }
+  }
+
+  if(bleKeyboard.isConnected() && digitalRead(BUTTON_PIN) == HIGH && !keylock) {
+
+    Serial.println("Sending 'KEY_RETURN'...");
+
+    auto key = KEY_RETURN;
+    bleKeyboard.write(key);
+
+    keylock = 1;
+
+  }
+
+}
+
+void connectionStatus() {
+
+  if (SerialBT.connected()) {
+    SerialBT.println("Connected to serial!");
+  }
+  
+  if(bleKeyboard.isConnected() && !isConnected) {
+    Serial.println("Connected!");
+    isConnected = 1;
+  }
+
+  if(!bleKeyboard.isConnected() && isConnected) {
+    Serial.println("Device disconnected! Searching for connections...");
+    isConnected = 0;
+  }
+
 }
